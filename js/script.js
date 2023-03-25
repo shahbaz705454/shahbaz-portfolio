e72d966e8a55cc089ac470021b5959f8bb91e9c9
shad <shad705454@gmail.com>
2023-03-25 17:16:22 +0530

    some bugs removed

--- js/script.js
+++ js/script.js
@@ -1,22 +1,48 @@
-// Scroll Section 
+// toggle icon nav bar
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+// Scroll Section
 
 let Sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
-window.onscroll =() =>{
-    Sections.forEach(sec =>{
+window.onscroll = () => {
+    Sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop-150;
+        let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
-        
-        if (top>=offset && top < offset + height) {
-            navLinks.forEach(links =>{
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a [href*=' + id + ']').classList.add('active');  
-            
             });
-            
+            document.querySelector("header nav a[href*='" + id + "']").classList.add('active');
         };
     });
-};
+
+
+
+
+    // STICKY NAV BAR
+
+let header = document.querySelector('header');
+header.classList.toggle('sticky', window.scrollY > 100);
+
+
+
+// Remove toggle icon when click on navbar links(scroll)
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
+}; 
+
+
+
+
