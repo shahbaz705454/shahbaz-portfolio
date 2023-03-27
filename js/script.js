6e624901d33a489c395bfa6de6563afbd7f7e39e
shad <shad705454@gmail.com>
2023-03-27 15:06:08 +0530

    all new features added

--- js/script.js
+++ js/script.js
@@ -44,5 +44,23 @@ navbar.classList.remove('active');
 }; 
 
 
+// SCROLL REVEAL 
+
+ScrollReveal({ 
+    
+    reset: true,
+    distance: '80px',
+    duration : 2000,
+    delay : 200
+
+ });
+
+ ScrollReveal().reveal('.home-content, .heading', { origin:'top' });
+ ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form ', { origin:'bottom' });
+ ScrollReveal().reveal('.home h3', { origin:'left' });
+
+
+
+
 
 
